c07b956286ef92bd609bc8069d5b48bc5134e855
aleeza101 <eqq9wc@virginia.edu>
2024-04-03 22:08:46 -0400

    created s3 resources for website

--- lib/aws-cdk-s3-website-stack.ts
+++ lib/aws-cdk-s3-website-stack.ts
@@ -1,19 +1,34 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs';
+import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
+import * as path from "path";
+import {Distribution, OriginAccessIdentity} from "aws-cdk-lib/aws-cloudfront";
+import {S3Origin} from "aws-cdk-lib/aws-cloudfront-origins";
+
 
 export class AwsCdkS3WebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'AwsCdkS3WebsiteQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    var websitebucket = new s3.Bucket(this, "website-bucket", {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      accessControl: s3.BucketAccessControl.PRIVATE,
     });
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: websitebucket,
+      sources: [Source.asset(path.resolve(__dirname, '../dist'))]
+    })
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    websitebucket.grantRead(originAccessIdentity);
+    new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+      origin: new S3Origin(websitebucket, {originAccessIdentity}),
+      },
+    })
+  } 
+} 
 
-    const topic = new sns.Topic(this, 'AwsCdkS3WebsiteTopic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
-}
